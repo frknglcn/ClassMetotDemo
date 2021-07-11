f2e1becc5a1ee79ae284540999d9f35504a40c3e
furka <furka@DESKTOP-23QIGPC>
2021-07-11 16:07:49 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEKleme(Musteri musteri)
+        {
+            Console.WriteLine("Musteri EKlendi : "+musteri.Ad+" "+musteri.Soyad);
+        }
+        public void MusteriListeleme(Musteri[] musteriler)
+        {
+            Console.WriteLine("Musteriler Listeleniyor");
+            foreach (var musteri  in musteriler)
+            {
+                
+                Console.WriteLine(musteri.Ad+musteri.Soyad);
+            }
+           // Console.WriteLine("Musteri EKlendi : " + musteri.Ad + " " + musteri.Soyad);
+        }
+        public void MusteriSİlme(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Silindi : " + musteri.Ad + " " + musteri.Soyad);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Furkan";
+            musteri1.Soyad = "Gülcan";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Gülcan";
+            musteri2.Soyad = "Furkan";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEKleme(musteri1);
+            musteriManager.MusteriEKleme(musteri2);
+            musteriManager.MusteriSİlme(musteri2);
+            musteriManager.MusteriSİlme(musteri2);
+
+
+            musteriManager.MusteriListeleme(musteriler);
+
+        }
+    }
+}
